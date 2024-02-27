b80bb6bcc6df5e6f2ba3d8f37fe325a89114c44d
Michael <mpierson00@gmail.com>
2024-02-26 21:05:42 -0500

    added js function for initiating game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 var words = ['local', 'attributes', 'intervals', 'append', 'objects'];
 var selectedWord = '';
-var timeleft = 120;
+var timeLeft = 120;
 var timerID;
 var wordDisplay = document.getElementById('wordToGuess');
 var alphabetDisplay = document.getElementById('alphabet');
 var timerDisplay = document.getElementById('timer');
 var startGameButton = document.getElementById('startGame');
 
+function initGame() {
+selectedWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+timeLeft = 120;
+timerDisplay.textContent = timeLeft;
+wordDisplay.innerHTML = '';
+alphabetDisplay.innerHTML = '';
+generateAlphabetButtons();
+}
