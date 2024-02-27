7e46309a77e8e8ac3d7d86870e4e1a3a906ba6e0
Michael <mpierson00@gmail.com>
2024-02-26 20:58:21 -0500

    added var to js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var words = ['local', 'attributes', 'intervals', 'append', 'objects'];
+var selectedWord = '';
+var timeleft = 120;
+var timerID;
+var wordDisplay = document.getElementById('wordToGuess');
+var alphabetDisplay = document.getElementById('alphabet');
+var timerDisplay = document.getElementById('timer');
+var startGameButton = document.getElementById('startGame');
+
